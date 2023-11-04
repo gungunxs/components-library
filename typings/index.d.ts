133a696db1bf404b98896381c6dcf1780ddf174a
gungunxs <3170733179@qq.com>
2023-11-04 18:24:43 +0800

    移植项目配置文件和类型文件

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,7 +2,48 @@
 
 interface IAppOption {
   globalData: {
-    userInfo?: WechatMiniprogram.UserInfo,
+    themeMode:string,
   }
-  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
+  userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback
+}
+
+interface IResult<T> {
+  code: number,
+  msg: string
+  data?: T
+}
+
+// 默认 tap 事件传递
+interface ITapEvent<T = any> {
+  currentTarget: { dataset: T; };
+}
+
+// 默认 tap 事件传递
+interface IscrollEvent<T = any> {
+  detail: {
+    scrollLeft: T,
+    scrollTop: T,
+    scrollHeight: T,
+    scrollWidth: T,
+    deltaX: T,
+    deltaY: T
+  }
+}
+
+// 分页 page 参数
+interface IPage<T = any> {
+  currentPage: number,
+  index: number | undefined,
+  pageSize: number,
+  totalPage?: number,
+  totalCount?: number,
+  list: Array<T>,
+  params?: any,
+  sortColumn?: string,
+  sortMethod?: string
+}
+
+// 通用 string 对象类型
+interface StringReflectI {
+  [key: string]: string
 }
