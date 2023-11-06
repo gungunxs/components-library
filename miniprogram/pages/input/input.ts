cc03b7eb3f95c6db0fe9558a025e9807696f03ee
gungunxs <3170733179@qq.com>
2023-11-06 20:38:41 +0800

    修改tips的大小

--- miniprogram/pages/input/input.ts
+++ miniprogram/pages/input/input.ts
@@ -5,9 +5,271 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isFocus:false,
+    inputValue:null,
+    // State属性值
+    inputListKey:[
+      "default","active","filled","readonly","error"
+    ],
+    inputList:{
+      "default":[
+        // 是否显示hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"none",
+          state:"default"
+        },
+        {
+          title:"Label",
+          hint:"可以自定义的hint文字",
+          showHint:true,
+          iconPlacement:"none",
+          state:"default"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"leading",
+          state:"default"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"trailing",
+          state:"default"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"none",
+          state:"default"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"leading",
+          state:"default"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"trailing",
+          state:"default"
+        },
+      ],
+      "active":[
+        // 是否显示hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"none",
+          state:"active"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"leading",
+          state:"active"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"trailing",
+          state:"active"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"none",
+          state:"active"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"leading",
+          state:"active"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"trailing",
+          state:"active"
+        },
+      ],
+      "filled":[
+        // 是否显示hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"none",
+          state:"filled"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"leading",
+          state:"filled"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"trailing",
+          state:"filled"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"none",
+          state:"filled"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"leading",
+          state:"filled"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"trailing",
+          state:"filled"
+        },
+      ],
+      "readonly":[
+        // 是否显示hint
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:true,
+          iconPlacement:"none",
+          state:"readonly"
+        },
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:true,
+          iconPlacement:"leading",
+          state:"readonly"
+        },
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:true,
+          iconPlacement:"trailing",
+          state:"readonly"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:false,
+          iconPlacement:"none",
+          state:"readonly"
+        },
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:false,
+          iconPlacement:"leading",
+          state:"readonly"
+        },
+        {
+          title:"Label",
+          hint:"自定义hint",
+          showHint:false,
+          iconPlacement:"trailing",
+          state:"readonly"
+        },
+      ],
+      "error":[
+        // 是否显示hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"none",
+          state:"error"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"leading",
+          state:"error"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:true,
+          iconPlacement:"trailing",
+          state:"error"
+        },
+        // 无hint
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"none",
+          state:"error"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"leading",
+          state:"error"
+        },
+        {
+          title:"Label",
+          hint:"Hint or Error Message",
+          showHint:false,
+          iconPlacement:"trailing",
+          state:"error"
+        },
+      ]
+    },
   },
-
+  /**
+   * 聚焦事件
+   */
+  focusInput(e: { detail: { value: any } }) {
+    this.setData({
+      isFocus:true,
+      inputValue:e.detail.value
+    })
+  },
+  /**
+   * 失焦事件
+   */
+  unFoucsInput() {
+    this.setData({
+      isFocus:false
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- miniprogram/pages/toast/toast.ts
+++ miniprogram/pages/toast/toast.ts
@@ -5,15 +5,109 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    showToast1:false,
+    showToast:false,
+    toastListKey:[
+      'icon','no-icon'
+    ],
+    toastList:{
+      // 有icon
+      "icon":[
+        {
+          Icon:true,
+          iconPath:"loading-primary",
+          message:"Message显示 Capition不显示 Icon显示且默认",
+          caption:"",
+          showCapition:false
+        },
+        {
+          Icon:true,
+          iconPath:"setting",
+          message:"Message显示 Capition不显示 Icon显示为自定义",
+          showCaption:false,
+          caption:""
+        },
+        {
+          Icon:true,
+          iconPath:"loading-primary",
+          message:"Message显示 Capition显示 Icon显示为默认",
+          showCaption:true,
+          caption:"Message显示 Capition显示 Icon显示为默认"
+        },
+        {
+          Icon:true,
+          iconPath:"setting",
+          message:"Message显示 Capition不显示 Icon显示为自定义",
+          showCaption:true,
+          caption:"Message显示 Capition不显示 Icon显示为自定义"
+        },
+      ],
+      // 无icon
+      "no-icon":[
+        {
+          Icon:false,
+          iconPath:"loading-primary",
+          message:"Message显示 Capition不显示 Icon显示且默认",
+          caption:"",
+          showCapition:false
+        },
+        {
+          Icon:false,
+          iconPath:"setting",
+          message:"Message显示 Capition不显示 Icon显示为自定义",
+          showCaption:false,
+          caption:""
+        },
+        {
+          Icon:false,
+          iconPath:"loading-primary",
+          message:"Message显示 Capition显示 Icon显示为默认",
+          showCaption:true,
+          caption:"Message显示 Capition显示 Icon显示为默认"
+        },
+        {
+          Icon:false,
+          iconPath:"setting",
+          message:"Message显示 Capition不显示 Icon显示为自定义",
+          showCaption:true,
+          caption:"Message显示 Capition不显示 Icon显示为自定义"
+        },
+      ],
+    }
   },
-
+  /**
+   * 手动关闭toast的操作
+   */
+  showToast() {
+    let toast = this.selectComponent("#mannerToast")
+    this.setData({
+      showToast:true
+    })
+    toast.showToast("手动触发toast 通过设置showToast方法，配合showToast赋值","需要通过showToast来延时控制，否则消失时无动画","loading-primary")
+  },
+  hiddenToast(){
+    let toast = this.selectComponent("#mannerToast")
+    toast.hiddenToast()
+    setTimeout(()=>{
+      this.setData({
+        showToast:false,
+      })
+    },0.25*1000)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-    let toast = this.selectComponent("#toast");
-    toast.showToast("哈哈哈哈哈哈哈哈哈哈哈","棍棍棍棍棍棍","loading-primary")
+    let toast = this.selectComponent("#autoToast");
+    this.setData({
+      showToast1:true
+    })
+    toast.showToastAuto("toast自动弹出，在onShow或onLoad中初始化，调用showToastAuto方法","关闭toast的延时为(duration+0.25)*1000","loading-primary",5)
+    setTimeout(()=>{
+      this.setData({
+        showToast1:false
+      })
+    },5.25*1000)
   },
 
   /**
