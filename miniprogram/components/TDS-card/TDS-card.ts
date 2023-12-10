ec317b1461c82f5bd004f3976ef054e315f67daa
gungunxs <3170733179@qq.com>
2023-12-10 17:06:31 +0800

    增加card不限制字数二级标题、不限制截断文本副标题

--- miniprogram/components/TDS-card/TDS-card.ts
+++ miniprogram/components/TDS-card/TDS-card.ts
@@ -20,6 +20,11 @@ Component({
       type:String,
       value:"https://fmin.oss-cn-hangzhou.aliyuncs.com/fmin-ad/56d8e1ce-微信图片_20211229141800.jpg"
     },
+    // 是否展示一级标题区域
+    showTitleList1:{
+      type:Boolean,
+      value:true
+    },
     // 是否显示Text1
     showText1:{
       type:Boolean,
@@ -45,6 +50,26 @@ Component({
       type:String,
       value:"棍棍先生棍棍先生"
     },
+    // 是否展示二级标题区域
+    showTitleList2:{
+      type:Boolean,
+      value:true
+    },
+    // Text1颜色
+    Title2Text1Color:{
+      type:String,
+      value:"--Primary"
+    },
+    // 标题文字text1 该元素宽度固定 用于展示序号 最多三位数
+    Title2Text1:{
+      type:String,
+      value:"01"
+    },
+    // 标题文字text1 该元素宽度固定 用于展示序号 最多三位数
+    Title2Text2:{
+      type:String,
+      value:"这是二级标题这是二级标题这是二级标题这是二级标题这是二级标题这是二级标题这是二级标题这是二级标题这是二级标题"
+    },
     // 是否显示副标题 默认不显示
     showSubTitle:{
       type:Boolean,
@@ -62,6 +87,27 @@ Component({
     },
     // 副标题中的图片url
     SubImgUrl:{
+      type:String,
+      value:"https://fmin.oss-cn-hangzhou.aliyuncs.com/fmin-ad/56d8e1ce-微信图片_20211229141800.jpg"
+    },
+    // 副标题2为无限制文本区域
+    // 是否显示副标题2 默认不显示
+    showSubTitle2:{
+      type:Boolean,
+      value:false
+    },
+    // 副标题2文本 
+    SubTitle2:{
+      type:String,
+      value:"这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题这是副标题"
+    },
+    // 是否显示副标题2中的图片
+    showSubImg2:{
+      type:Boolean,
+      value:false
+    },
+    // 副标题2中的图片url
+    SubImgUrl2:{
       type:String,
       value:"https://fmin.oss-cn-hangzhou.aliyuncs.com/fmin-ad/56d8e1ce-微信图片_20211229141800.jpg"
     },
